d8495e23736e40616aedd7deb8075d3739245f8f
Koen Schmeets <k.schmeets@gmail.com>
2012-04-02 18:56:07 +0300

    Changing "key" column to "role" on the roles table

--- migrations/2012_01_24_205457_add_authority_tables.php
+++ migrations/2012_01_24_205457_add_authority_tables.php
@@ -19,7 +19,7 @@ class Authority_add_authority_tables {
 		{
 			$table->create();
 			$table->increments('id');
-			$table->string('key');
+			$table->string('role');
 		});
 
 		Schema::table('roles_users', function($table)
